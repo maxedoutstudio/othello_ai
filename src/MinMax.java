3df16d11aa0a4a191d02c8251f91d969a88feb68
maxedoutstudio <max@maxedoutstudio.com>
2016-11-27 19:04:37 -0500

    Fixed up min-max to use proper alpha beta algorithm. To test.

--- src/MinMax.java
+++ src/MinMax.java
@@ -10,6 +10,10 @@ public abstract class MinMax {
 	protected String maxColor;
 	protected String minColor;
 
+    //Finals for alphabeta
+    static final public int MAX_INT = 2147483647;
+    static final public int MIN_INT = -2147483648;
+
 	MinMax(){} //default constructor
 	MinMax(Board board, int depth, String maxColor){
 
@@ -39,102 +43,150 @@ public abstract class MinMax {
 	public int[] getMove(){
 
         State currentState = new State(board, null, minColor, null);
-        int currentDepth = 0;
 
-        while (currentState != null){
+        // Calls the recursive alpha beta algorithm, which takes the current state
+        int heuristicValue = alphabeta(currentState, 0, MIN_INT, MAX_INT, true );
 
-            // Creates a new state from the current state, sets it to current
-            State nextState = currentState.getNextState();
+        // Gets the move to make from the recursive alpha beta algorithm
+        int[] move = currentState.getNextMove();
 
-            if (nextState == null){
-                // Checks the parent
-                if (currentState.getParent() == null){
-                    // In this case, there are no possible moves, so return null
-                    return null;
-                }
+        // Returns the move to make
 
-                // Case where there are no children left for this state, goes up one
-                currentState = currentState.getParent();
-                currentDepth--;
-                continue;
-            }
+        return move;
 
-            // Increments the depth
-            currentDepth++;
+//        while (currentState != null){
+//
+//            // Creates a new state from the current state, sets it to current
+//            State nextState = currentState.getNextState();
+//
+//            // Alpha beta pruning part
+//            if (currentState.getValue() != null &&
+//                    currentState.getNextColor().equals(minColor) &&
+//                    currentState.getValue() < 0
+//
+//                    ){
+//
+//            }
+//
+//            if (nextState == null){
+//                // This means that are are no more children left
+//
+//                // Checks the parent
+//                if (currentState.getParent() == null){
+//                    if (currentState.getValue() == null){
+//                        // In this case, the tree has been exhausted and there are no possible moves.
+//                        // Thus null is returned
+//                        return null;
+//                    } else {
+//                        // Returns the move that results in the state.
+//                        return currentState.getNextMove();
+//                    }
+//                } else {
+//
+//                    // Case where there are no children left for this state, goes up one
+//                    currentState = currentState.getParent();
+//                    currentDepth--;
+//                    continue;
+//                }
+//
+//            }
+//
+//            // Increments the depth
+//            currentDepth++;
+//
+//            // If the currentDepth is at the evaluation depth, evaluate
+//            if (currentDepth == depth){
+//                // Sets the value of the state
+//                nextState.setValue(e(nextState.getBoardState()));
+//            }
+//
+//            if (nextState.getValue() != null){
+//                // Pushes up the value to the parent
+//                if (currentState.getValue() == null){
+//                    // Case where parent has no value
+//                    currentState.setValue(nextState.getValue());
+//                } else if (currentState.getNextColor().equals(maxColor)){
+//                    // Case where the parent is max
+//                    if (nextState.getValue() > currentState.getValue()){
+//                        // Only sets the parents value to the childs if it is better than what is already there
+//                        currentState.setValue(nextState.getValue());
+//                    }
+//                } else {
+//                    // Case where the parent is min
+//                    if (nextState.getValue() < currentState.getValue()){
+//                        // Only sets the parents value to the childs if it is worse than what is already there
+//                        currentState.setValue(nextState.getValue());
+//                    }
+//                }
+//
+//                currentState = nextState;
+//            }
+//        }
+	}
 
-            // If the currentDepth is at the evaluation depth, evaluate
-            if (currentDepth == depth){
-                // Sets the value of the state
-                nextState.setValue(e(nextState.getBoardState()));
+    /**
+     * Alpha beta method, based on the course slides p.25 (slightly modified). Returns the
+     * @param state
+     * @param currentDepth
+     * @param alpha
+     * @param beta
+     * @param maximizing
+     * @return
+     */
+	public int alphabeta(State state, int currentDepth, int alpha, int beta, boolean maximizing){
+
+        State nextState = state.getNextState();
+
+        if (currentDepth == depth || nextState == null){
+            if (state.getValue() == null){
+                int eval = e(state.getBoardState());
+                state.setValue(eval);
+                return eval;
             } else {
-
-            }
-
-            if (currentState.getValue() != null){
-
-                // Pushes up the value to the parent
-                State parent = currentState.getParent();
-                if (parent.getValue() == null){
-                    // Case where parent has no value
-                    parent.setValue(currentState.getValue());
-                } else if (parent.getNextColor().equals(maxColor)){
-                    // Case where the parent is max
-                    if (currentState.getValue() > parent.getValue()){
-                        // Only sets the parents value to the childs if it is better than what is already there
-                        parent.setValue(currentState.getValue());
-                    }
-                } else {
-                    // Case where the parent is min
-                    if (currentState.getValue() < parent.getValue()){
-                        // Only sets the parents value to the childs if it is worse than what is already there
-                        parent.setValue(currentState.getValue());
-                    }
-                }
-
-                currentDepth--;
-                currentState = parent;
+                return state.getValue();
             }
         }
+        if (maximizing){
+            int v = MIN_INT;
+            while(nextState != null){
+                int oldV = v;
 
+                // Sets the value to the max of its children
+                v = Math.max(v, alphabeta(nextState, currentDepth - 1, alpha, beta, false));
 
+                if (v!= oldV){
+                    // Means that the value change, used to keep track of what move should be made
+                    state.setNextMove(nextState.getMove());
+                }
 
+                alpha = Math.max(alpha, v);
+                if (beta <= alpha){
+                    break; // Beta cut-off point
+                }
+                nextState = state.getNextState();
+            }
+            return v;
+        } else {
+            int v = MAX_INT;
+            while(nextState != null){
+                int oldV = v;
 
-        ArrayList<ArrayList<State>> tree = new ArrayList<>();
+                // Sets the value to the min of its children
+                v = Math.min(v, alphabeta(nextState, currentDepth - 1, alpha, beta, true));
 
-		//generate tree for all states depending on depth
-		for(int i = -1; i<depth-1; i++){
-			if (tree.isEmpty()){
-				ArrayList<State> states = board.getPossibleStates(maxColor);
-				tree.add(states);
-			} else {
-				ArrayList<State> lvl = tree.get(i);
-				ArrayList<State> states = new ArrayList<>();
-
-				for (int j = 0; j < lvl.size(); j++){
-					if (i % 2 == 0){
-						// Occurs on min lvl
-						states.addAll(lvl.get(j).getBoardState().getPossibleStates(minColor));
-					} else {
-						// Occurs on max lvl
-						states.addAll(lvl.get(j).getBoardState().getPossibleStates(maxColor));
-					}
-				}
-				tree.add(states);
-			}
-		}
-
-		//start from last layer loop upwards
-		for(int i = tree.size()-1; i>=0; i--){
-
-			//loop inner
-			for(int j=0; j<tree.get(i).size(); j++){
-
-				//Runs the evaluation function for the state
-				tree.get(i).get(j).setValue(e(tree.get(i).get(j).getBoardState()));
-			}
-		}
-		return new int[]{x,y};
-	}
+                if (v!= oldV){
+                    // Means that the value change, used to keep track of what move should be made
+                    state.setNextMove(nextState.getMove());
+                }
+                beta = Math.min(beta, v);
+                if (beta <= alpha){
+                    break; // Alpha cut-off point
+                }
+                nextState = state.getNextState();
+            }
+            return v;
+        }
+    }
 
 
 }
